b4817e5b6e32faca0edf53ec1825a534b422a95b
Tripp Davis <trippdavis3@gmail.com>
2015-04-07 14:35:55 -0700

    require users to be signed in to view groups

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,15 +3,21 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   def current_user
-    User.find_by_session_token(session[:token])
+    @current_user ||= User.find_by_session_token(session[:token])
   end
 
   def login_user!(user)
     session[:token] = user.reset_session_token
+    @current_user = user
   end
 
   def logout!
     current_user.reset_session_token
     session[:token] = nil
+    @current_user = nil
+  end
+
+  def require_signed_in
+    redirect_to new_session_url unless current_user
   end
 end
--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,6 @@
 class GroupsController < ApplicationController
+  before_action :require_signed_in
+
   def index
     @groups = Group.all
   end
