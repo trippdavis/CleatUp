77cdfb5ce02666dbfb9013906f19ca4bbfcb52b5
Tripp Davis <trippdavis3@gmail.com>
2015-04-07 11:59:59 -0700

    added session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,25 @@
 class SessionsController < ApplicationController
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.find_by_credentials(
+      params[:user][:username],
+      params[:user][:password]
+    )
+
+    if @user
+      login_user!(@user)
+      redirect_to @user
+    else
+      flash.now[:errors] = ["Invalid username/password combination"]
+      render :new
+    end
+  end
+
+  def destroy
+    logout!
+    redirect_to new_session_url
   end
 end
