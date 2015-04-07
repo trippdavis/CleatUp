931bd85891e2fd292312c0881476c5aaa909063f
Tripp Davis <trippdavis3@gmail.com>
2015-04-07 13:19:23 -0700

    basic user and group seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create(username: "username1", password: "password1")
+User.create(username: "username2", password: "password2")
+User.create(username: "username3", password: "password3")
+
+Group.create(organizor_id: 1, title: "Group 1", description: "First Group")
+Group.create(organizor_id: 1, title: "Group 2", description: "Second Group")
+Group.create(organizor_id: 1, title: "Group 3", description: "Third Group")
+Group.create(organizor_id: 2, title: "Group 4", description: "Fourth Group")
