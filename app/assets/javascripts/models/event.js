cbf79965e1915641d1592153356bffb8c2b00333
Tripp Davis <trippdavis3@gmail.com>
2015-04-08 18:56:38 -0700

    parse date_time date in backbone

--- app/assets/javascripts/models/event.js
+++ app/assets/javascripts/models/event.js
@@ -6,6 +6,14 @@ CleatUp.Models.Event = Backbone.Model.extend({
   urlRoot: "/api/events",
 
   parse: function (payload) {
+    if (payload.date_time) {
+      var dt = payload.date_time;
+      this.date = dt.split("T")[0];
+      this.time = dt.split("T")[1].split(".")[0];
+
+      delete payload.date_time;
+    }
+
     if (payload.organizer) {
       this.organizer = payload.organizer;
       delete payload.organizer;
