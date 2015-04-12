b60b23aa6b43596594aa059f7244c722b426cefa
Tripp Davis <trippdavis3@gmail.com>
2015-04-12 15:31:45 -0700

    clicking group item navigates to show page

--- app/assets/javascripts/views/groups/group_item.js
+++ app/assets/javascripts/views/groups/group_item.js
@@ -1,11 +1,17 @@
 CleatUp.Views.GroupItem = Backbone.View.extend({
   template: JST["groups/item"],
 
-  className: "group-item",
+  events: {
+    "click .group-item": "groupShow"
+  },
 
   render: function () {
     var content = this.template({ group: this.model });
     this.$el.html(content);
     return this;
+  },
+
+  groupShow: function () {
+    Backbone.history.navigate("groups/" + this.model.id, { trigger: true });
   }
 });
