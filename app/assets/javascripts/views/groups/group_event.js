d0d9583c1bb257f24351d6da47f03baf937c8db3
Tripp Davis <trippdavis3@gmail.com>
2015-04-14 11:22:23 -0700

    group events link to event show page

--- app/assets/javascripts/views/groups/group_event.js
+++ app/assets/javascripts/views/groups/group_event.js
@@ -1,9 +1,17 @@
 CleatUp.Views.GroupEvent = Backbone.View.extend({
   template: JST["groups/event"],
 
+  events: {
+    "click .group-event": "showEvent"
+  },
+
   render: function () {
     var content = this.template({ event: this.model });
     this.$el.html(content);
     return this;
+  },
+
+  showEvent: function () {
+    Backbone.history.navigate("events/" + this.model.id, { trigger: true });
   }
 });
