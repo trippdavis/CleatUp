ebd8c1443045b92e269e57b1b7ec49b0915aff1c
Tripp Davis <trippdavis3@gmail.com>
2015-04-16 14:53:33 -0700

    group landing includes all indexes

--- app/assets/javascripts/views/groups/groups_index.js
+++ app/assets/javascripts/views/groups/groups_index.js
@@ -1,22 +1,10 @@
 CleatUp.Views.GroupsIndex = Backbone.View.extend({
-  initialize: function (options) {
-    var type = options.type;
-    var interest_id = options.interest_id;
-    this.collection.fetch({
-      data: {
-        type: type,
-        interest_id: interest_id
-      }
-    });
-    this.listenTo(this.collection, "sync", this.render);
-  },
-
   template: JST['groups/index'],
 
   render: function () {
     var content = this.template();
     this.$el.html(content);
-    var $div = $(".group-items");
+    var $div = this.$el.find(".group-items");
     this.collection.each(function (group) {
       var view = new CleatUp.Views.GroupItem({ model: group });
       $div.append(view.render().$el);
--- app/assets/javascripts/views/groups/groups_landing.js
+++ app/assets/javascripts/views/groups/groups_landing.js
@@ -5,58 +5,57 @@ CleatUp.Views.GroupsLanding = Backbone.View.extend({
     var content = this.template();
     this.$el.html(content);
     this.$currentButton = this.$el.find(".joined-groups");
+    this.createdGroups();
     this.joinedGroups();
+    this.otherGroups();
     return this;
   },
 
-  events: {
-    "click button": "switchIndex"
-  },
+  // events: {
+  //   "click button": "switchIndex"
+  // },
 
-  switchIndex: function (event) {
-    var $button = $(event.target);
-    $button.prop("disabled", true);
-    if ($button.hasClass("created-groups")) {
-      this.createdGroups();
-    } else if ($button.hasClass("joined-groups")) {
-      this.joinedGroups();
-    } else if ($button.hasClass("other-groups")) {
-      this.otherGroups();
-    }
-    this.$currentButton.prop("disabled", false);
-    this.$currentButton = $button;
-  },
+  // switchIndex: function (event) {
+  //   var $button = $(event.target);
+  //   $button.prop("disabled", true);
+  //   if ($button.hasClass("created-groups")) {
+  //     this.createdGroups();
+  //   } else if ($button.hasClass("joined-groups")) {
+  //     this.joinedGroups();
+  //   } else if ($button.hasClass("other-groups")) {
+  //     this.otherGroups();
+  //   }
+  //   this.$currentButton.prop("disabled", false);
+  //   this.$currentButton = $button;
+  // },
 
   createdGroups: function () {
-    var view = new CleatUp.Views.GroupsIndex({
-      collection: this.collection,
-      type: "created"
+    this.collection.fetch({
+      data: { type: "created" },
+      success: function () {
+        var view = new CleatUp.Views.GroupsIndex({ collection: this.collection });
+        this.$el.find(".created-groups-list").html(view.render().$el);
+      }.bind(this)
     });
-    this._swapIndex(view);
   },
 
   joinedGroups: function () {
-    var view = new CleatUp.Views.GroupsIndex({
-      collection: this.collection,
-      type: "joined"
+    this.collection.fetch({
+      data: { type: "joined" },
+      success: function () {
+        var view = new CleatUp.Views.GroupsIndex({ collection: this.collection });
+        this.$el.find(".joined-groups-list").html(view.render().$el);
+      }.bind(this)
     });
-    this._swapIndex(view);
   },
 
   otherGroups: function () {
-    var view = new CleatUp.Views.GroupsIndex({
-      collection: this.collection,
-      type: "other"
+    this.collection.fetch({
+      data: { type: "other" },
+      success: function () {
+        var view = new CleatUp.Views.GroupsIndex({ collection: this.collection });
+        this.$el.find(".other-groups-list").html(view.render().$el);
+      }.bind(this)
     });
-    this._swapIndex(view);
-  },
-
-  _swapIndex: function (view) {
-    if (this.currentIndex) {
-      this.currentIndex.remove();
-    }
-
-    this.$el.find(".groups-list").html(view.render().$el);
-    this.currentIndex = view;
   }
 });
